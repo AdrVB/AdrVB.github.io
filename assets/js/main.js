e4412f9e8988e54f6a05ab0aa99d128b34e37433
Adriano Baptista <adriano.vb99@gmail.com>
2020-12-22 14:38:30 +0000

    First Setup

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const showMenu = (toggleId, navaId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navaId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.button', { delay: 200 })
+sr.reveal('.home__social-icon', { delay: 400 })
+
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', { delay: 200 })
+sr.reveal('.about__text', { delay: 400 })
